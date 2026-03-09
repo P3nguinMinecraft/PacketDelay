e497a18bc4e9b394934baae242974c507c88923f
penguin <penguin586970@gmail.com>
2026-03-09 10:08:50 -0400

    1.21.6-1.21.8 vers

--- src/main/java/io/github/penguin/packetdelay/mixin/ClientCommonPacketListenerImplMixin.java
+++ src/main/java/io/github/penguin/packetdelay/mixin/ClientCommonPacketListenerImplMixin.java
@@ -1,6 +1,6 @@
-package com.packetdelay.mixin;
+package io.github.penguin.packetdelay.mixin;
 
-import com.packetdelay.PacketDelay;
+import io.github.penguin.packetdelay.PacketDelay;
 import net.minecraft.client.multiplayer.ClientCommonPacketListenerImpl;
 import net.minecraft.network.protocol.Packet;
 import org.spongepowered.asm.mixin.Mixin;
